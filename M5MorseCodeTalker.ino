a4e4bdf7e2af13da09eabd729db66a14634cc99d
Katsuya Endoh <studio@enkatsu.org>
2025-01-03 18:03:45 +0900

    add Serial and LED

--- M5MorseCodeTalker.ino
+++ M5MorseCodeTalker.ino
@@ -6,6 +6,8 @@ const uint16_t unitTime = 1200 / wpm;
 const uint32_t CODE_QUEUE_LENGTH = 128;
 QueueHandle_t codeQueue;
 
+const int LED_PIN = 22;
+
 void morseTask(void* arg) {
   Code code;
   while (1) {
@@ -14,14 +16,18 @@ void morseTask(void* arg) {
       switch (code) {
         case Code::SHORT:
           M5.Speaker.tone(440, unitTime * 1);
+          digitalWrite(LED_PIN, HIGH);
           delay(unitTime * 1);
           M5.Speaker.mute();
+          digitalWrite(LED_PIN, LOW);
           delay(unitTime * 1);
           break;
         case Code::LONG:
           M5.Speaker.tone(440, unitTime * 3);
+          digitalWrite(LED_PIN, HIGH);
           delay(unitTime * 3);
           M5.Speaker.mute();
+          digitalWrite(LED_PIN, LOW);
           delay(unitTime * 1);
           break;
         case Code::SPACE:
@@ -40,14 +46,20 @@ void morseTask(void* arg) {
 void setup() {
   Serial.begin(115200);
   M5.begin();
+  pinMode(LED_PIN, OUTPUT);
   codeQueue = xQueueCreate(CODE_QUEUE_LENGTH, sizeof(Code));
   xTaskCreatePinnedToCore(morseTask, "morseTask", 4096, NULL, 1, NULL, 0);
 }
 
 void loop() {
   M5.update();
-
   if (M5.BtnA.wasReleased()) {
     encodeMessageToMorseAndEnqueue(codeQueue, "hello");
+  }
+
+  if (Serial.available() > 0) {
+    String message = Serial.readStringUntil('\n');
+    Serial.println(message);
+    encodeMessageToMorseAndEnqueue(codeQueue, message.c_str());
   }
 }
